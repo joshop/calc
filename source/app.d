35ce533840dd2ba799c5788618e93ad883ca2486
joshop <joshop4646@gmail.com>
2020-10-17 21:43:04 -0400

    Added units... to an extent Added support for dimensional quantities like meters and meters per second They work with +-*/ and ^. So far three can actually be obtained for use: meters (m), seconds (s) and grams (g). There are no compound units yet and no SI prefixes or non-SI units like feet. Builtin functions just completely drop the units which will be fixed... eventually

--- source/app.d
+++ source/app.d
@@ -1,4 +1,5 @@
 import std.stdio: writeln, readln, write, writefln;
+import std.array: assocArray;
 import std.algorithm.iteration: sum, fold, map;
 import std.conv: to;
 import std.complex: Complex, sin, cos, tan, abs, sqrt, exp, log, log10;
@@ -21,9 +22,82 @@ Function < ~(identifier) '(' Add ')'
 Literal <- ~([0-9]+ ('.' [0-9]+)*)
 Variable <- ~(identifier)
 `));
-struct United { // i.e. with units... units to come later
+struct United { // i.e. with units... units being added now!!
 	Complex!double dless; // the dimensionless part
-	alias dless this;
+	alias dless this; // hopefully won't get used much?
+	double[string] dimension; // the dimension: key is the si unit symbol, value is the exponent of it (undefined is zero)
+	this(Complex!double initDless, double[string] initDimension) {
+		dless = initDless;
+		dimension = initDimension;
+	}
+	this(Complex!double initDless) {
+		dless = initDless;
+		dimension = null;
+	}
+	string hrDimension(bool nameEmpty = false) { // human readable dimension information, nameEmpty = true means say "unit" for the empty unit
+		string num, denom;
+		foreach (string unit, double exponent; dimension) {
+			if (exponent > 0) {
+				num ~= unit;
+				if (exponent != 1) {
+					num ~= "^" ~ to!string(exponent);
+				}
+				num ~= " ";
+			} else if (exponent < 0) {
+				denom ~= unit;
+				if (exponent != -1) {
+					denom ~= "^" ~ to!string(-exponent);
+				}
+				denom ~= " ";
+			}
+		}
+		if (denom == "" && num == "") { // i.e. null dimension
+			return nameEmpty ? "unit" : "";
+		} else if (denom == "") {
+			return num[0..$-1];
+		} else if (num == "") {
+			return "1 / " ~ denom[0..$-1];
+		} else {
+			return num[0..$-1] ~ " / " ~ denom[0..$-1];
+		}
+	}
+	United opBinary(string op)(United other) if (op == "+" || op == "-") { // case 1: unlike units error here
+		if (dimension != other.dimension) {
+			dbInfo = format!("Tried to %s units %s and %s")(op, dimension, other.dimension);
+			throw new Exception("Operation " ~ op ~ " can't operate on units " ~ hrDimension(true) ~ " and " ~ other.hrDimension(true) ~ ".");
+		}
+		return United(mixin("dless" ~ op ~ "other.dless"), dimension);
+	}
+	United opBinary(string op)(United other) if (op == "*" || op == "/") { // case 2: units multiply or divide here
+		double[string] finalDim = dimension.dup;
+		foreach(string unit, double exponent; other.dimension) {
+			if (unit in finalDim) {
+				finalDim[unit] += op == "*" ? exponent : -exponent; // negate exponent in other for division
+				if (finalDim[unit] == 0) {
+					finalDim.remove(unit); // hrDimension can't handle ^0 so that needs to be removed
+				}
+			} else {
+				finalDim[unit] = op == "*" ? exponent : -exponent;
+			}
+		}
+		return United(mixin("dless" ~ op ~ "other.dless"), finalDim);
+	}
+	United opBinary(string op)(United other) if (op == "^^") { // case 3: error if exponent is not dimensionless
+		if (other.dimension.length) { // is not dimensionless
+			throw new Exception("Unable to raise to the power of non-dimensionless unit " ~ other.hrDimension() ~ ".");
+		}
+		auto finalDim = dimension.dup; 
+		foreach(string unit, double exponent; finalDim) {
+			if (other.dless.im) { // this should error for now?
+				throw new Exception("Unable to raise non-dimensionless quantity with units " ~ hrDimension() ~ " to a complex power.");
+			}
+			finalDim[unit] *= other.dless.re;
+		}
+		return United(dless ^^ other.dless, finalDim);
+	}
+	United opUnary(string op)() if (op == "-") {
+		return United(-dless, dimension);
+	}
 }
 United[string] variables; // user-defined
 United[string] constants; // not user-defined
@@ -38,7 +112,7 @@ United evaluate(ParseTree expr) { // recursively parse the tree
 			if (expr.children.length == 2) {
 				if (expr.children[0].matches[0] in constants) {
 					dbInfo = format!("%s constant value: %g")(expr.children[0].matches[0], constants[expr.children[0].matches[0]]);
-					throw new Exception(expr.children[0].matches[0] ~ " is a constant.");
+					throw new Exception(expr.children[0].matches[0] ~ " is a constant or unit.");
 				}
 				United result = evaluate(expr.children[1]);
 				variables[expr.children[0].matches[0]] = result;
@@ -47,34 +121,34 @@ United evaluate(ParseTree expr) { // recursively parse the tree
 				return evaluate(expr.children[0]);
 			}
 		case "Expression.Add":
-			return United(fold!((a, b) => a + b)(map!evaluate(expr.children)));
+			return fold!((a, b) => a + b)(map!evaluate(expr.children));
 		case "Expression.MulRep":
 			if (expr.matches[0] == "+") {
 				return evaluate(expr.children[0]);
 			} else {
-				return United(-evaluate(expr.children[0]));
+				return -evaluate(expr.children[0]);
 			}
 		case "Expression.Mul":
-			return United(fold!((a, b) => a * b)(map!evaluate(expr.children)));
+			return fold!((a, b) => a * b)(map!evaluate(expr.children));
 		case "Expression.ExpRep":
 			if (expr.matches[0] == "*") {
 				return evaluate(expr.children[0]);
 			} else {
-				return United(1/evaluate(expr.children[0]));
+				return United(Complex!double(1))/evaluate(expr.children[0]); 
 			}
 		case "Expression.Exp":
-			return United(fold!((a, b) => a ^^ b)(map!evaluate(expr.children)));
+			return fold!((a, b) => a ^^ b)(map!evaluate(expr.children)); 
 		case "Expression.NegRep":
 			return evaluate(expr.children[0]);
 		case "Expression.Neg":
 			if (expr.matches[0] == "-") {
-				return United(-evaluate(expr.children[0]));
+				return -evaluate(expr.children[0]);
 			} else {
 				return evaluate(expr.children[0]);
 			}
 		case "Expression.Atom":
 			if (expr.children.length > 1) {
-				return United(fold!((a, b) => a * b)(map!evaluate(expr.children)));
+				return fold!((a, b) => a * b)(map!evaluate(expr.children));
 			} else {
 				return evaluate(expr.children[0]);
 			}
@@ -93,9 +167,9 @@ United evaluate(ParseTree expr) { // recursively parse the tree
 			if (expr.matches[0] in functs) {
 				return functs[expr.matches[0]](evaluate(expr.children[0]));
 			} else if (expr.matches[0] in variables) {
-				return United(variables[expr.matches[0]]*evaluate(expr.children[0]));
+				return variables[expr.matches[0]]*evaluate(expr.children[0]);
 			} else if (expr.matches[0] in constants) {
-				return United(constants[expr.matches[0]]*evaluate(expr.children[0]));
+				return constants[expr.matches[0]]*evaluate(expr.children[0]);
 			} else {
 				dbInfo = format!("Functions defined: %s")(functs);
 				throw new Exception("Function " ~ expr.matches[0] ~ " not found.");
@@ -121,6 +195,9 @@ void main() {
 	constants["pi"] = United(Complex!double(PI));
 	constants["i"] = United(sqrt(Complex!double(-1))); // maybe add support for j?
 	constants["e"] = United(Complex!double(E));
+	constants["m"] = United(Complex!double(1), ["m": 1]);
+	constants["s"] = United(Complex!double(1), ["s": 1]);
+	constants["g"] = United(Complex!double(1), ["g": 1]);
 	auto terminal = Terminal(ConsoleOutputType.linear);
 	while (true) {
 		try {
@@ -138,14 +215,15 @@ void main() {
 			}
 			auto result = evaluate(tree);
 			if (dbEnabled) {
-				terminal.writefln("=> Exact result: %g", result);
+				terminal.writefln("=> Exact result: %g of units %s", result, result.dimension);
+				terminal.writefln("=> Human-readable units: %s", result.hrDimension());
 			}
 			if (approxEqual(result.re, 0) && !approxEqual(result.im, 0)) {
-				terminal.writefln("=> %.14gi", quantize!(10, -14)(result.im));
+				terminal.writefln("=> %.14gi %s", quantize!(10, -14)(result.im), result.hrDimension());
 			} else if (approxEqual(result.im, 0)) {
-				terminal.writefln("=> %.14g", quantize!(10, -14)(result.re));
+				terminal.writefln("=> %.14g %s", quantize!(10, -14)(result.re), result.hrDimension());
 			} else {
-				terminal.writefln("=> %.14g", result);
+				terminal.writefln("=> %.14g %s", result, result.hrDimension());
 			}
 		} catch (UserInterruptionException) { // ctrl-c from Terminal
 			break;
